6120ce3e04a3904dc813fb1df0e2c501b2f7d268
Shado <94776476+shevtsov-dmitry@users.noreply.github.com>
2022-09-03 13:31:57 +0300

    finished with SetTimeout

--- js/script.js
+++ js/script.js
@@ -3,16 +3,15 @@ let burgersOrder = document.querySelector('.order-block')
 let sixBurgers = burgersOrder.lastElementChild.children
 
 for (const e of sixBurgers) {
+    e.style.zIndex = 0;
     e.addEventListener('mouseenter',()=>{
-        e.style.transform = 'scale(1.05)'
-        e.style.zIndex = 0;
+        e.style.zIndex = 1
+        e.style.transform = 'scale(1.03)'
         e.addEventListener('mouseleave',()=>{
             e.style.transform = 'scale(1)'
-            setTimeout(
-                e.style.zIndex = 1 , 200
-                
-            )
-            
+            setTimeout(()=>{
+            e.style.zIndex = 0
+            }, 200)
         })
     })
 }
