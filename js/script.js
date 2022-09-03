44321b935c124533e38040167e7d013d64ec41d3
Shado <94776476+shevtsov-dmitry@users.noreply.github.com>
2022-09-03 13:02:44 +0300

    currently at transform animation in order section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-let test = document.querySelector('.wrapper-for-burgers')
+let burgersOrder = document.querySelector('.order-block')
 
-console.log(test.children)
+let sixBurgers = burgersOrder.lastElementChild.children
+
+for (const e of sixBurgers) {
+    e.addEventListener('mouseenter',()=>{
+        e.style.opacity = 0.5
+        e.style.transform = 'scale(1.1)'
+        e.style.transition = 'transform 0.5 linear'
+        e.addEventListener('mouseleave',()=>{
+            e.style.transform = 'scale(1)'
+        })
+    })
+}
