053e928c242842acf155a26d33bb8483f164d3b3
Shado <94776476+shevtsov-dmitry@users.noreply.github.com>
2022-09-03 18:25:28 +0300

    there is still much to be done

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
 let burgersOrder = document.querySelector('.order-block')
-
 let sixBurgers = burgersOrder.lastElementChild.children
 
 for (const e of sixBurgers) {
     e.style.zIndex = 0;
-    
+    console.log(e.children[0].children[2])
+    // ------------
+    let button = e.children[0].children[2]
+    button.addEventListener('mouseenter',()=>{
+        button.style.backgroundColor = 'white';
+        button.style.color = '#292929'
+        button.addEventListener('mouseleave',()=>{
+            button.style.backgroundColor = '#292929';
+            button.style.color = 'white'
+        })
+    })
+    // ------------
     e.addEventListener('mouseenter',()=>{
         e.style.zIndex = 1
-        // // e.lastChild.style.transform = 'scale(1.03)'
-        // e.lastChild.style.filter = 'contrast(1.5)'
-        // ------------
+        e.children[1].style.transform = 'scale(1.03)'
+        e.children[1].style.filter = 'contrast(1.5)'
+        e.children[0].style.opacity = '1'
 
         // ------------
 
         e.addEventListener('mouseleave',()=>{
-            // e.lastChild.style.transform = 'scale(1)'
+            e.children[1].style.transform = 'scale(1)'
             setTimeout(()=>{
             e.style.zIndex = 0
             }, 300)
-                // ------------
-                // e.lastChild.style.filter = 'contrast(1)'
-                // ------------
+            e.children[1].style.filter = 'contrast(1)'
+            e.children[0].style.opacity = '0'
         })
     })
 }
