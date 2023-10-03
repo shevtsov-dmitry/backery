f7d2ee39481e0830fe546245443a3f24e76cc897
shevtsov dmitry <shadowstakeus28@gmail.com>
2023-10-03 20:28:39 +0300

    feat: initialize spring backend

---
+++ src/test/java/com/burgershop/website/WebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burgershop.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
