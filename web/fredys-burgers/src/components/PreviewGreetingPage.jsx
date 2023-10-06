b3c3854861e01de696e869e00b64ff1e072958c4
shevtsov dmitry <shadowstakeus28@gmail.com>
2023-10-06 17:43:27 +0300

    style: migrate 1 block PreviewGreetingPage.jsx

---
+++ web/fredys-burgers/src/components/PreviewGreetingPage.jsx
@@ -0,0 +1,13 @@
+function PreviewGreetingPage(){
+
+    return(
+        <>
+            <div className="entrance">
+                <div className="bakery-background"></div>
+                <h1>Fredy's Burgers</h1>
+            </div>
+        </>
+    )
+}
+
+export default PreviewGreetingPage
--- web/fredys-burgers/src/main.jsx
+++ web/fredys-burgers/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './../style/pageLayout.css'
+import '../style/wholeHtmlPageCommonLayout.css'
+import './components/PreviewGreetingPage.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
