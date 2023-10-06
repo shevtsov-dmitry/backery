6697505ed8e139ca6ab68c66148ee58f401de554
shevtsov dmitry <shadowstakeus28@gmail.com>
2023-10-06 20:24:54 +0300

    feat: create template objects for each burger in Order block

--- web/fredys-burgers/src/components/order/Order.jsx
+++ web/fredys-burgers/src/components/order/Order.jsx
@@ -47,7 +47,8 @@ function Order(){
             "potato",
             "cabbagge"
         ],
-        price: "19,99$"
+        price: "19,99$",
+        imageUrl: "../../../imgs/burger-up-left.png",
     })
 
     burgers.push({
@@ -61,7 +62,8 @@ function Order(){
             "keegen efadis",
             "kiligan greece"
         ],
-        price: "21,99$"
+        price: "21,99$",
+        imageUrl: "../../../imgs/burger-up-mid.png",
     })
 
     burgers.push({
@@ -72,7 +74,8 @@ function Order(){
             "waley faick",
             "keegen efadis"
         ],
-        price: "19,99$"
+        price: "19,99$",
+        imageUrl: "../../../imgs/burger-up-right.png",
     })
 
     burgers.push({
@@ -84,7 +87,8 @@ function Order(){
             "keegen efadis",
             "kiligan greece"
         ],
-        price: "34.99$"
+        price: "34.99$",
+        imageUrl: "../../../imgs/burger-bot-left.png",
     })
 
     burgers.push({
@@ -96,7 +100,8 @@ function Order(){
             "colosal suffle",
             "waley faick"
         ],
-        price: "34.99$"
+        price: "34.99$",
+        imageUrl: "../../../imgs/burger-bot-mid.png",
     })
 
     burgers.push({
@@ -110,7 +115,8 @@ function Order(){
             "keegen efadis",
             "kiligan greece"
         ],
-        price: "29,99$"
+        price: "29,99$",
+        imageUrl: "../../../imgs/burger-bot-right.png",
     })
 
     return(
@@ -118,107 +124,7 @@ function Order(){
             <div className="order-block">
                 <h2>Order</h2>
                 <ul>
-                    <li>
-                        <div className="burg-buy">
-                            <h3>Shooter</h3>
-                            <article>
-                                <ul>
-                                    <li>fidju fish</li>
-                                    <li>colosal suffle alacoto</li>
-                                    <li>waley faick</li>
-                                    <li>keegen efadis</li>
-                                    <li>kiligan greece</li>
-                                    <li>potato</li>
-                                    <li>cabbagge</li>
-                                </ul>
-                            </article>
-                            <button>19,99$</button>
-                        </div>
-                        <img src="../../../imgs/burger-up-left.png" alt=""></img>
-                    </li>
-                    <li>
-                        <div className="burg-buy">
-                            <h3>Accomplishers</h3>
-                            <article>
-                                <ul>
-                                    <li>potato</li>
-                                    <li>cabbagge</li>
-                                    <li>fidju fish</li>
-                                    <li>colosal suffle</li>
-                                    <li>waley faick</li>
-                                    <li>keegen efadis</li>
-                                    <li>kiligan greece</li>
-                                </ul>
-                            </article>
-                            <button>21,99$</button>
-                        </div>
-                        <img src="../../../imgs/burger-up-mid.png" alt=""></img>
-                    </li>
-                    <li>
-                        <div className="burg-buy">
-                            <h3>Belong</h3>
-                            <article>
-                                <ul>
-                                    <li>fidju fish</li>
-                                    <li>colosal suffle</li>
-                                    <li>waley faick</li>
-                                    <li>keegen efadis</li>
-                                </ul>
-                            </article>
-                            <button>19,99$</button>
-                        </div>
-                        <img src="../../../imgs/burger-up-right.png" alt=""></img>
-                    </li>
-                    <li>
-                        <div className="burg-buy">
-                            <h3>Whole Time</h3>
-                            <article>
-                                <ul>
-                                    <li>potato</li>
-                                    <li>cabbagge</li>
-                                    <li>waley faick</li>
-                                    <li>keegen efadis</li>
-                                    <li>kiligan greece</li>
-                                </ul>
-                            </article>
-                            <button>34.99$</button>
-                        </div>
-                        <img src="../../../imgs/burger-bot-left.png" alt=""></img>
-                    </li>
-                    <li>
-                        <div className="burg-buy">
-                            <h3>Ketchupy floring</h3>
-                            <article>
-                                <ul>
-                                    <li>potato</li>
-                                    <li>cabbagge</li>
-                                    <li>fidju fish</li>
-                                    <li>colosal suffle</li>
-                                    <li>waley faick</li>
-                                </ul>
-                            </article>
-                            <button>39,99$</button>
-                        </div>
-                        <img src="../../../imgs/burger-bot-mid.png" alt=""></img>
-                    </li>
-                    <li>
-                        <div className="burg-buy">
-                            <h3>Loffye's</h3>
-                            <article>
-                                <ul>
-                                    <li>potato</li>
-                                    <li>cabbagge</li>
-                                    <li>fidju fish</li>
-                                    <li>colosal suffle</li>
-                                    <li>waley faick</li>
-                                    <li>keegen efadis</li>
-                                    <li>kiligan greece</li>
-                                </ul>
-                            </article>
-                            <button>29,99$</button>
-                        </div>
-                        <img src="../../../imgs/burger-bot-right.png" alt=""></img>
-                    </li>
+                    <li></li>
                 </ul>
             </div>
         </>
